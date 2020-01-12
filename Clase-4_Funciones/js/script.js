61f51d5c3151d26bdd1ff3690c0c5da538d20139
Juan Diego <dippons78@gmail.com>
2020-01-12 16:54:10 -0500

    Clase 4 - Funciones

---
+++ Clase-4_Funciones/js/script.js
@@ -0,0 +1,12 @@
+var nombre = 'Juan', edad = 22
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Fer', 20)
+imprimirEdad('Alejandra', 18)
+imprimirEdad(28, 'Lina')
+imprimirEdad('Karen')
+imprimirEdad()
