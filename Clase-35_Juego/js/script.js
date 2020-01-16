8c58f752110a75b50ff000c371b6e9378915b129
Juan Diego <dippons78@gmail.com>
2020-01-16 16:36:13 -0500

    Clase 42

--- Clase-35_Juego/js/script.js
+++ Clase-35_Juego/js/script.js
@@ -5,15 +5,80 @@ const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
 
 class Juego {
+
     constructor() {
         this.inicializar()
+        this.generarSecuencia()
+        this.siguienteNivel()
     }
     
     inicializar() {
+        this.elegirColor = this.elegirColor.bind(this)
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
     }
+
+    generarSecuencia(){
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    }
+
+    siguienteNivel(){
+        this.iluminarSecuencia()
+        this.agregarEventosClick()
+    }
+
+    transformarNumeroAColor(num){
+        switch(num) {
+            case 0:
+                return 'celeste'
+            case 1:
+                return 'violeta'
+            case 2: 
+                return 'naranja'
+            case 3:
+                return 'verde'
+        }
+    }
+
+    
+
+    iluminarSecuencia(){
+        for (let i = 0; i < this.nivel; i++){
+            let color = this.transformarNumeroAColor(this.secuencia[i])
+            setTimeout(() => this.iluminarColor(color), 1000 * i)
+        }
+    }
+
+    
+
+    iluminarColor(c){
+        this.colores[c].classList.add('light')
+        setTimeout(() => this.apagarColor(c), 350)
+    }
+
+    apagarColor(c){
+        this.colores[c].classList.remove('light')
+    }
+
+    agregarEventosClick(){
+        this.colores.celeste.addEventListener('click', this.elegirColor)
+        this.colores.verde.addEventListener('click', this.elegirColor)
+        this.colores.violeta.addEventListener('click', this.elegirColor)
+        this.colores.naranja.addEventListener('click', this.elegirColor)
+    }
+
+    elegirColor(ev){
+        
+    }
+
 }
 
 function empezarJuego() {
-    var juego = new Juego()
+    window.juego = new Juego()
 }
